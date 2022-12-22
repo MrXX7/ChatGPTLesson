7cad1090c267de4319674d72c4fcee88afe08790
Oncu <oohancan@gmail.com>
2022-12-22 12:13:09 +0300

    Initial Commit

---
+++ ChatGPTLesson/ChatGPTLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTLessonApp.swift
+//  ChatGPTLesson
+//
+//  Created by Oncu Can on 22.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
